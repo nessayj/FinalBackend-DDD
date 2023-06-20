8b5324f6348f13d1f6482c449a97eae6acbb165c
taek <miloe0117@gmail.com>
2023-06-20 13:10:35 +0900

    Entity Member 추가

--- FinalDDD/src/main/java/com/DDD/entity/Member.java
+++ FinalDDD/src/main/java/com/DDD/entity/Member.java
@@ -1,16 +1,40 @@
 package com.DDD.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table
 @Getter @Setter @ToString
 @NoArgsConstructor
 public class Member {
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String name;
+    private String password;
+    private String tel;
+    private String nickName;
+    private String instagram;
+    private String profileImg;
+    private String backgroundImg;
+
+//    @Enumerated(EnumType.STRING)
+//    private Authority authority;
+
+    @Builder
+    public Member( String email, String name, String password, String tel, String nickName, String instagram, String profileImg, String backgroundImg) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.tel = tel;
+        this.nickName = nickName;
+        this.instagram = instagram;
+        this.profileImg = profileImg;
+        this.backgroundImg = backgroundImg;
+    }
+
 }
